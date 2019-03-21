166e44a74a16a31df2b1d2bcd5c582056b9f09bb
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-03-21 16:18:59 +0100

    uj komponensek es route-o

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { ReaderComponent } from './reader/reader.component';
+import { WriterComponent } from './writer/writer.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'writer', component: WriterComponent},
+  {path: 'reader', component: ReaderComponent},
+
+  {path: '**', component: WriterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { WriterComponent } from './writer/writer.component';
+import { ReaderComponent } from './reader/reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    WriterComponent,
+    ReaderComponent
   ],
   imports: [
     BrowserModule,
